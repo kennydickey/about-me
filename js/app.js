13231955b2b3f1a09d305821111c85ee07aac0ee
seatacken <57050116+seatacken@users.noreply.github.com>
2019-12-05 16:43:56 -0800

    added onclick function to quiz

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-debugger;
+
 console.log('js is working');
 
 alert('Welcome to my site! Please complete this short quiz about me');
@@ -7,8 +7,9 @@ var userName = prompt('But first, please tell me your name?');
 console.log('users name is' + userName);
 alert('Welcome ' + userName + '!');
 
+
 var score = 0;
-var Q1 = function () {
+function q1() {
   var answer1 = prompt('Do I live in Bellevue?');
   console.log('user entered' + answer1);
   if (answer1.toLowerCase() === 'no' || answer1.toLowerCase() === 'n') {
@@ -18,9 +19,8 @@ var Q1 = function () {
     alert('Incorrect');
   }
 }
-// Q1();
 
-var Q2 = function () {
+function q2() {
   var answer2 = prompt('Am I from New York?');
   console.log('user entered' + answer2);
   if (answer2.toLowerCase() === 'no' || answer2.toLowerCase() === 'n') {
@@ -32,7 +32,7 @@ var Q2 = function () {
 }
 // Q2();
 
-var Q3 = function () {
+function q3() {
   var answer3 = prompt('Am I a Lab Technician?');
   console.log('user entered' + answer3);
   if (answer3.toLowerCase() === 'yes' || answer3.toLowerCase() === 'y') {
@@ -42,9 +42,9 @@ var Q3 = function () {
     alert('Incorrect');
   }
 }
-// Q3();
 
-var Q4 = function () {
+
+function q4() {
   var answer4 = prompt('Do I use Unity for game development?');
   console.log('user entered' + answer4);
   if (answer4.toLowerCase() === 'yes' || answer4.toLowerCase() === 'y') {
@@ -53,10 +53,9 @@ var Q4 = function () {
   } else {
     alert('Incorrect');
   }
-};
-// Q4();
+}
 
-var Q5 = function () {
+function q5() {
   var answer5 = prompt('Is my goal to become a football commentary engineer?');
   console.log('user entered' + answer5);
   if (answer5.toLowerCase() === 'no' || answer5.toLowerCase() === 'n') {
@@ -65,11 +64,11 @@ var Q5 = function () {
   } else {
     alert('Incorrect');
   }
-};
-// Q5();
+}
+
 
 //multiple choice
-var MQ1 = function () {
+function mq1() {
   var answers = ['France', 'Russia', 'Bolivia', 'Brazil', 'England', 'Zambia'];
   for (var i = 0; i < answers.length; i = i + 1) {
     console.log(answers[i]);
@@ -82,12 +81,12 @@ var MQ1 = function () {
   } else {
     alert('Sorry, wrong answer :(');
   }
-};
+}
 // MQ1();
 
 
 // number guessing
-var NQ1 = function () {
+function nq1() {
   var trys = 4;
   var guess = prompt('Guess a number (1-10)');
   function newPrompt() {
@@ -110,12 +109,14 @@ var NQ1 = function () {
     }
   }
 }
-// NQ1();
 
-var test = function () {
-  Q1(); Q2(); Q3(); Q4(); MQ1(); NQ1();
+function end(){
+  console.log('user score is' + score);
+  alert('Your score is ' + score + ' , Thank you ' + userName + ' for completing this quiz, have a nice day!');
 }
-test();
 
-console.log('user score is' + score);
-alert('Your score is ' + score + ' , Thank you ' + userName + ' for completing this quiz, have a nice day!');
+onclick = function test1() {
+  q1(); q2(); q3(); q4(); q5(); mq1(); nq1(); end();
+};
+
+
